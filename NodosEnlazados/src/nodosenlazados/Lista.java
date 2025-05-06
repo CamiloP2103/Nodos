4cd938c1890d26a96cfdd7468fc872ed460b351a
Sala 5 <sala5@FI-LabSistemas.ucatolica.loc>
2025-05-06 08:57:43 -0500

    intento 1

---
+++ NodosEnlazados/src/nodosenlazados/Lista.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nodosenlazados;
+
+/**
+ *
+ * @author sala5
+ */
+public class Lista {
+    
+    private class Nodo <Item>{
+    Item item;
+    Nodo next;
+}
+}
